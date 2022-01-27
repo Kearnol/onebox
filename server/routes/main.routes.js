1db01f9f340ee06bc1a6b98dd68f2d2e65b3f335
Devin Kearns <devin.kearns@outlook.com>
2022-01-27 15:37:57 -0700

    configured server to accept webhooks

--- server/routes/main.routes.js
+++ server/routes/main.routes.js
@@ -2,4 +2,6 @@ const MainController = require('../controllers/main.controller')
 
 module.exports = (app) => {
     app.get("/index", MainController.index)
+    app.get("/webhook", MainController.webhookCheck)
+    app.post("/webhook", MainController.webhook)
 }
